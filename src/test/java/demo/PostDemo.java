731882a0a69fcf8f41a6a2a6d73e112b43ccdc5a
zhengyp <1501143640@qq.com>
2018-05-04 20:49:25 +0800

    fefef

--- src/test/java/demo/PostDemo.java
+++ src/test/java/demo/PostDemo.java
@@ -2,27 +2,29 @@ package demo;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.response.Response;
+import org.testng.annotations.Test;
 
 import static com.jayway.restassured.RestAssured.given;
 
 public class PostDemo {
-    public void Get(){
-        RestAssured.baseURI = "http://music.163.com";
-        RestAssured.basePath = "/playlist";
+    @Test
+    public void jsonBodyTest(){
+        RestAssured.baseURI = "http://10.10.10.175";
+        RestAssured.basePath = "/api/login/";
+        String psd = tools.md5.encrypByMd5Jar("xujiuming");
+        String jsonBody = "{\n" +
+                "                \"phone\":\"18688886666\",\n" +
+                "                \"password\":\"123456\"\n" +
+                "            }";
         Response response = given()
-                .header("Content-Type","text/html:charset-utf-8")
-                .param("id","30592034")
-                .when().log().all().get();
+                .header("Content-Type","application/json")
+//                .param("id","30592034")
+                .body(jsonBody)
+                .when().log().all().post();
         int responseCode = response.statusCode();
         String responseContent = response.getBody().print();
         System.out.println("响应状态码："+String.valueOf(responseCode));
         System.out.println(responseContent);
 
-
-
-    }
-    public static void main(String args[]){
-        PostDemo getDemo = new PostDemo();
-        getDemo.Get();
     }
 }
